8801a276ab97290f255295feae14ea2e44280be7
hidekekw <caioshimohiro@hotmail.com>
2024-10-23 08:03:57 -0300

    Alteracoes 22

--- teste.py
+++ teste.py
@@ -5,43 +5,45 @@ pygame.font.init()
 font = pygame.font.SysFont(None, 24)
 
 def display_coordinates(screen, x, y):
-    text_surface = font.render(f"X: {x}, Y: {y-75}", True, (0, 0, 0))  # Black text
+    text_surface = font.render(f"X: {x}, Y: {y}", True, (0, 0, 0))  # Black text
     screen.blit(text_surface, (10, 70))  # Display the text at position (10, 70)
 
+class Polygon:
+    def __init__(self, x, y, color, yMin, yMax):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.yMin = yMin
+        self.yMax = yMax
+
 class ColorPicker:
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, surf):
         self.rect = pygame.Rect(x, y, w, h)
         self.image = pygame.Surface((w, h))
-        self.image.fill((255, 255, 255))
-        self.rad = h // 2
-        self.pwidth = w - self.rad * 2
-        for i in range(self.pwidth):
+        self.width = w
+        self.height = h
+        for i in range(w):
             color = pygame.Color(0)
-            color.hsla = (int(360 * i / self.pwidth), 100, 50, 100)
-            pygame.draw.rect(self.image, color, (i + self.rad, h // 3, 1, h - 2 * h // 3))
+            color.hsla = (int(360 * i / w), 100, 50, 100)
+            pygame.draw.rect(self.image, color, (i, 0, 1, h))
+        surf.blit(self.image, self.rect)
         self.p = 0
 
     def get_color(self):
         color = pygame.Color(0)
-        color.hsla = (int(self.p * self.pwidth), 100, 50, 100)
+        color.hsla = (int(self.p * 360), 100, 50, 100)
         return color, self.p
 
     def set_color(self, p, screen):
         self.p = p
 
-
     def update(self):
         moude_buttons = pygame.mouse.get_pressed()
         mouse_pos = pygame.mouse.get_pos()
         if moude_buttons[0] and self.rect.collidepoint(mouse_pos):
-            self.p = (mouse_pos[0] - self.rect.left - self.rad) / self.pwidth
+            self.p = (mouse_pos[0] - self.rect.left) / self.width
             self.p = (max(0, min(self.p, 1)))
 
-    def draw(self, surf):
-        surf.blit(self.image, self.rect)
-        center = self.rect.left + self.rad + self.p * self.pwidth, self.rect.centery
-        color, trash = self.get_color()
-        pygame.draw.circle(surf, color, center, self.rect.height // 2)
 
 def is_in_poly(point, polygon):
     x, y = point
@@ -61,10 +63,6 @@ def is_in_poly(point, polygon):
         p1x, p1y = p2x, p2y
 
     return inside
-
-def draw_polygon(screen, points, color):
-    if len(points) > 2:
-        pygame.draw.polygon(screen, color, points, 1) 
 
 def scanline_calc(points, y_min, y_max):
     nv = len(points) # Numero de vertices
@@ -101,7 +99,7 @@ def fillpoly(screen, points, color, y_min, y_max):
     scanlines = scanline_calc(points, y_min, y_max)
     row = y_min
     for sl in scanlines:
-        for i in range(0, len(sl), 2):
+        for i in range(0, len(sl)-1, 2):
             rect = pygame.Rect(sl[i], row, sl[i+1]-sl[i]+1, 1)
             pygame.draw.rect(screen, color, rect)
         row = row+1
@@ -110,79 +108,102 @@ def fillpoly(screen, points, color, y_min, y_max):
 pygame.init()
 
 WHITE = (255, 255, 255)
+GREY = (220, 220, 220)
 width, height = 1000, 800
+boxWidth, boxHeight = 1000, 720
+boxRect = pygame.Rect(0, 80, 1000, 720)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Fillpoly")
 
-screen.fill(WHITE)
+screen.fill(GREY)
+screen.fill(WHITE, boxRect)
 clock = pygame.time.Clock()
-cp = ColorPicker(20, 20, 400, 50)
+cp = ColorPicker(20, 30, 400, 20, screen)
 
 points = []
 poly = []
 poly_colors = []
 selected = False
+sel_poly = []
 edit_mode = False
 change_color = False
+del_poly = False
 
 running = True
 while running:
     clock.tick(100)
     color, color_p = cp.get_color()[:3]
+    pos = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            
-            if edit_mode:
-                for i, polygon in enumerate(poly):
-                    if is_in_poly(pos, polygon):
-                        y_max = 0
-                        y_min = width
-                        for point in polygon:
-                            if point[1] < y_min:
-                                y_min = point[1]
-                            elif point[1] > y_max:
-                                y_max = point[1]
-                                sel_poly = i
-                        fillpoly(screen, polygon, color, y_min, y_max)
-            else:                
-                if pos[1] > 150:
+        elif event.type == pygame.MOUSEBUTTONDOWN:         
+            if not edit_mode:                
+                if pos[1] > 80:
                     points.append(pos)  # Add point to the list
                     pygame.draw.circle(screen, color, pos, 3)  # Mark the selected point
+            else:
+                yMin = width
+                yMax = 0
+                for i, polygon in enumerate(poly):
+                    if is_in_poly(pos, polygon):
+                        yMax = 0
+                        yMin = width
+                        for point in polygon:
+                            if point[1] < y_min:
+                                yMin = point[1]
+                            elif point[1] > y_max:
+                                yMax = point[1]
+                                sel_poly = i
+                        selected = True
+                        sel_poly = [i, yMin, yMax]
             
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:  # Press 'Enter' to finish the selection
-                
-                poly.append(points)
-                
-                y_max = 0
-                y_min = width
-                for point in points:
-                    if point[1] < y_min:
-                        y_min = point[1]
-                    elif point[1] > y_max:
-                        y_max = point[1]
+            if event.key == pygame.K_RETURN:  # Press 'Enter' to finish the selection   
+                if len(points) >= 3:
+                    poly.append(points)
+                    
+                    y_max = 0
+                    y_min = width
+                    for point in points:
+                        if point[1] < y_min:
+                            y_min = point[1]
+                        elif point[1] > y_max:
+                            y_max = point[1]
 
-                fillpoly(screen, points, color, y_min, y_max)  # Draw the polygon from selected points
-                points = []
+                    fillpoly(screen, points, color, y_min, y_max)  # Draw the polygon from selected points
+                    poly_colors.append(color)
+                    points = []
 
             elif event.key == pygame.K_SPACE:
                 edit_mode = not edit_mode
 
+            elif event.key == pygame.K_DELETE:
+                if selected:
+                    poly.pop(sel_poly[0])
+                    poly_colors.pop(sel_poly[0])
+
             elif event.key == pygame.K_r:  # Press 'R' to reset the canvas
-                screen.fill(WHITE)
+                screen.fill(WHITE, boxRect)
                 points = []
                 poly = []
+            elif event.key == pygame.K_c:
+                change_color = not change_color
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+    if edit_mode:
+        #print(sel_poly)
+        if selected:
+            print(sel_poly[0])
+        if change_color and selected:
+            fillpoly(screen, poly[sel_poly[0]], color, sel_poly[1], sel_poly[2])
+        if del_poly:
+            poly.pop(i)
+
     pygame.draw.rect(screen, WHITE, (0, 60, 200, 40))  # Clear previous coordinates
-    display_coordinates(screen, mouse_x, mouse_y)
+    display_coordinates(screen, pos[0], pos[1])
 
     cp.update()
-    cp.draw(screen)
 
     pygame.display.flip()
 
